a80feb8032d5247907d65980479dd9b0761cc74e
hajar258 <hajar.mohammad285@gmail.com>
2023-09-17 20:46:02 +0300

    ADDED: mutations and new resolvers

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Mutation mean any change to be done on the data 
+ */
+
+
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import {typeDefs} from './schema.js'
@@ -15,8 +20,70 @@ const resolvers ={
         },
         authors(){
             return db.authors
+        },
+        // args to access any parameters passed to the query
+        // context ==> ued for supplying context values accross all of our resolvers uch as authentication information 
+        review(_,args,context){
+            return db.reviews.find((review)=> review.id === args.id
+            )
+        },
+        game(_,args,context){
+            return db.games.find((game)=> game.id === args.id
+            )
+        },
+        author(_,args,context){
+            return db.authors.find((author)=> author.id === args.id
+            )
+        },
+    },
+    Game:{
+        // parent ==> is a reference to the value returned by previous or parent resolver 
+        // in this case, it will look at the game resolver 
+        reviews(parent,){
+            return db.reviews.filter((r)=> r.game_id === parent.id)
+        },
+    },
+    Author:{
+        // parent ==> is a reference to the value returned by previous or parent resolver 
+        // in this case, it will look at the author resolver 
+        reviews(parent,){
+            return db.reviews.filter((r)=> r.author_id === parent.id)
+        },
+    },
+    Review:{
+        // parent ==> is a reference to the value returned by previous or parent resolver 
+        // in this case, it will look at the review resolver 
+        game(parent,){
+            return db.games.find((g)=> g.id === parent.game_id)
+        },
+        author(parent){
+            return db.authors.find((a)=> a.id === parent.author_id)
         }
-        
+    },
+    Mutation:{
+        deleteGame(parent, args, ){
+            db.games = db.games.filter((g)=> g.id != args.id)
+            return db.games
+        },
+        addGame(parent, args, ){
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random()*100000).toString()
+            }
+            db.games.push(game)
+            return game
+        },
+        updateGame(parent, args){
+            db.games= db.games.map((g)=>{
+                if(g.id === args.id){
+                    return {...g, ...args.edits}
+                }
+
+                return g
+            })
+            return db.games.find((g)=> g.id == args.id)
+        }
+
     }
 }
 
@@ -36,3 +103,5 @@ const {url} = await startStandaloneServer(server,{
 
 
 console.log("Server ready at port", 4000 );
+
+
--- schema.js
+++ schema.js
@@ -3,21 +3,32 @@ export const typeDefs = `#graphql
         id : ID!
         title: String!
         platform:[String!]!
+
+        # we need to create a resolver for this 
+        reviews:[Review!]
     }
     type Review{
         id: ID!
         rating:Int!
         content: String!
+
+
+        # we need to create a resolver for these
+        game: Game!
+        author: Author!
     }
 
     type Author{
         id: ID!
         name: String! 
         verified: Boolean!
+        
+        reviews:[Review!] # we need to create a resolver for this 
     }
 
 
-    # this is a must , it is not optional
+    # this is a must , it is not optional, this is to specify the end points of our api 
+    # this is the root query! like the root route
     type Query{
         # these are the endpoits that users can use to get the data 
         # this allow the usrrs to go to all the data that we have 
@@ -25,8 +36,28 @@ export const typeDefs = `#graphql
         games: [Game]
         authors: [Author]
 
-         
         # this is to land on a single one of data 
+        review(id: ID!):Review # what inside this () specify the parameter to be passed to the query
+        game(id: ID!):Game
+        author(id:ID!):Author
+    }
+
+    # here we will define the mutations 
+    type Mutation{
+        deleteGame(id:ID!):[Game]
+        addGame(game: AddGameInput!):Game
+        updateGame(id:ID!, edits: EditGameInput):Game
+
+    }
+
+    # thi is a collection of fields that can be used in a mutation a a single argumant
+    input AddGameInput{
+        title:String!
+        platform:[String!]!
+    }
+    input EditGameInput{
+        title:String
+        platform:[String!]
     }
 
 
